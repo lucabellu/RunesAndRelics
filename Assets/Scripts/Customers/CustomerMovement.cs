dd964bef3b97343343cebeb9f9ab4a10591eb38d
Luca Bellu <luca.bellu1@gmail.com>
2025-02-20 10:27:58 +0000

    Customer movement

--- Assets/Scripts/Customers/CustomerMovement.cs
+++ Assets/Scripts/Customers/CustomerMovement.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CustomerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //objectives
+    //
+    //on spawn in from manager, move to counter
+    // on sale finish, move to exit
+
+    [SerializeField] private Transform target;
+    private Vector3 spawnPos;
+    [SerializeField] private float moveSpeed;
+    private bool hasMadeSale = false;
+
+    private void Start()
     {
-        
+        spawnPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            hasMadeSale = true;
+        }
+
+        if (!hasMadeSale)
+        {
+            Vector3 currentPosition = transform.position;
+            Vector3 targetPosition = new Vector3(target.position.x, currentPosition.y, target.position.z);
+
+            // Move the object smoothly towards the target on the XZ plane
+            transform.position = Vector3.Lerp(currentPosition, targetPosition, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 currentPosition = transform.position;
+            Vector3 targetPosition = new Vector3(spawnPos.x, currentPosition.y, spawnPos.z);
+
+            // Move the object smoothly towards the target on the XZ plane
+            transform.position = Vector3.Lerp(currentPosition, targetPosition, moveSpeed * 0.25f * Time.deltaTime);
+            Destroy(gameObject, 5f);
+        }
     }
+
+
 }
