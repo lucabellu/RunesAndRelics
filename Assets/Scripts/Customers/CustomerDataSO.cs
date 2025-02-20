793c11315f5810d2e3d86364ea92a33cb0747a35
Luca Bellu <luca.bellu1@gmail.com>
2025-02-20 12:59:59 +0000

    ADD customer data SO

--- Assets/Scripts/Customers/CustomerDataSO.cs
+++ Assets/Scripts/Customers/CustomerDataSO.cs
@@ -3,5 +3,40 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CustomerDataSO", menuName = "Scriptable Objects/CustomerDataSO")]
 public class CustomerDataSO : ScriptableObject
 {
-    
+    public enum Race
+    {
+        HUMAN,
+        ELF,
+        ORC,
+        DWARF,
+        DEMON,
+        FAIRY,
+        UNDEAD
+    }
+
+    public enum Kingdom
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    public enum Occupation
+    {
+        BARD,
+        WARRIOR,
+        CLERIC,
+        DRUID,
+        RANGER,
+        WIZARD
+    }
+
+    public string customerName;
+    public string customerDialogue;
+    public int customerLevel;
+
+    public Race customerRace;
+    public Kingdom kingdom;
+    public Occupation occupation;
 }
