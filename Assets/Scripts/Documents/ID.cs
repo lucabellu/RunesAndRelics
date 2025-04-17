cab38d9f8800b45175e0fabdeef0582947e212bf
Luca Bellu <luca.bellu1@gmail.com>
2025-04-17 17:19:58 +0100

    Manifest recolour

--- Assets/Scripts/Documents/ID.cs
+++ Assets/Scripts/Documents/ID.cs
@@ -7,45 +7,17 @@ public class ID : Document
 
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI ageText;
-    [SerializeField] private TextMeshProUGUI raceText;
-    [SerializeField] private TextMeshProUGUI occupationText;
+    [SerializeField] private TextMeshProUGUI raceOccupationText;
 
-    [SerializeField] private Image customerImageDisplay;
-    [SerializeField] private Image sealDisplay;
-
-    [SerializeField] private bool isCustomSet = false;
-
-    [Header("Custom Data")]
-    [SerializeField] private string customName;
-    [SerializeField] private int customAge;
-    [SerializeField] private string customRaceText;
-    [SerializeField] private Occupation customOccupation;
-    [SerializeField] private Sprite customPortrait;
-    [SerializeField] private Sprite customSeal;
+    [SerializeField] private Image customerPortrait;
 
     protected override void Start()
     {
         base.Start();
 
-        if (!isCustomSet)
-        {
-            nameText.text = "Name: " + customerLogic.customerName;
-            ageText.text = "Age: " + customerLogic.customerAge.ToString();
-            raceText.text = "Race: " + customerLogic.customerRace.ToString();
-            occupationText.text = "Occupation: " + customerLogic.customerOccupation.ToString();
-
-            customerImageDisplay.sprite = customerLogic.customerPortrait;
-            sealDisplay.sprite = customerLogic.letterSeal;
-        }
-        else
-        {
-            nameText.text = "Name: " + customName;
-            ageText.text = "Guild: " + customAge.ToString();
-            raceText.text = "Race: " + customRaceText;
-            occupationText.text = "Occupation: " + customOccupation.ToString();
-
-            customerImageDisplay.sprite = customPortrait;
-            sealDisplay.sprite = customSeal;
-        }
+        nameText.text = customerLogic.customerName;
+        ageText.text = customerLogic.customerAge.ToString();
+        raceOccupationText.text = customerLogic.customerRace.ToString() + ", " + customerLogic.customerOccupation.ToString(); ;
+        customerPortrait.sprite = customerLogic.customerPortrait;
     }
 }
